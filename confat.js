902287b235fa094257fe8d16735c1b7838ebbb38
Nevt3 <95283773+Nevt3@users.noreply.github.com>
2021-12-11 23:14:43 +0800

    Update confat.js

--- confat.js
+++ confat.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // Other
-global.owner = ["62887435047326","62887435047326"]
-global.packname = 'ZeeoneOfc'
+global.owner = ["62881037923331","62881037923331"]
+global.packname = 'Nevt Ganz'
 global.youtube = 'https://youtube.com/c/ZeeoneOfc'
 global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
 global.sessionName = 'session'
